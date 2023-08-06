fa29ec974737a2a84cb30cf51e680d22038d8670
Daria Sotnikova <dasha.dasha5454@yandex.ru>
2023-08-06 19:51:19 +0300

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Дарья Сотникова on 06.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
