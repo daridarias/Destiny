635884875c23c63da15e71056a6229e59813e62e
Daria Sotnikova <dasha.dasha5454@yandex.ru>
2023-08-06 21:17:48 +0300

    initial commit

---
+++ Destini/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Дарья Сотникова on 06.08.2023.
+//
+
+import Foundation
---
+++ Destini/Model/StoryBrain.swift
@@ -0,0 +1,8 @@
+//
+//  StoryBrain.swift
+//  Destini
+//
+//  Created by Дарья Сотникова on 06.08.2023.
+//
+
+import Foundation
