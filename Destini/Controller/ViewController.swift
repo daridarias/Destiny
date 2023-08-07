94e366d0c304b03f93db42500d2b60301dd1c5da
Daria Sotnikova <dasha.dasha5454@yandex.ru>
2023-08-07 13:52:15 +0300

    update stories

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storyLabel: UILabel?
+    @IBOutlet weak var choice1Button: UIButton?
+    @IBOutlet weak var choice2Button: UIButton?
+    
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(sender.currentTitle ?? "")
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel?.text = storyBrain.stories[storyBrain.storyNumber].title
+        choice1Button?.setTitle(storyBrain.stories[storyBrain.storyNumber].choice1, for: .normal)
+        choice1Button?.titleLabel?.minimumScaleFactor = 0.5
+        choice1Button?.titleLabel?.adjustsFontSizeToFitWidth = true
+        
+        choice2Button?.setTitle(storyBrain.stories[storyBrain.storyNumber].choice2, for: .normal)
+        choice2Button?.titleLabel?.minimumScaleFactor = 0.5
+        choice2Button?.titleLabel?.adjustsFontSizeToFitWidth = true
+    }
 }
 
